f4e253d7ee40f1db17192854c26406ed26b74888
Jin <Dowha132@yonsei.ac.kr>
2024-12-01 17:12:05 +0900

    assignment13.3

--- PythonPractice/BestPythonBook/[SW_13.3]이의진_2021147049.py
+++ PythonPractice/BestPythonBook/[SW_13.3]이의진_2021147049.py
@@ -1,13 +1,25 @@
 import random as rd
+
+frequency = [0] * 45
+print("\n빈도수 카운트 전")
+print(frequency)
+
 a = range(1, 46)
 data = rd.choices(a, k=100)
 
 file_name = "ex_02.txt"
 with open(file_name, "w") as file:
-    for i in range(0, 100, 10):  # 0부터 90까지 10씩 증가
+    for i in range(0, 100, 10):
         line = ' '.join(str(num) for num in data[i:i+10])
         file.write(line + '\n')
 
-print(f"출력 ({file_name}):")
 with open(file_name, "r") as file:
     print(file.read())
+
+
+
+for num in data:
+    frequency[num-1] += 1
+
+print("빈도수 카운트 후")
+print(frequency)
