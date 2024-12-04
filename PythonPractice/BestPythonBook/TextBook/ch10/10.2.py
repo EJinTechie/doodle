5c66debc322e733a9b972bf90c05fb8b938a9a8c
Jin <Dowha132@yonsei.ac.kr>
2024-12-04 10:33:25 +0900

    filter fn age pr

--- PythonPractice/BestPythonBook/TextBook/ch10/10.2.py
+++ PythonPractice/BestPythonBook/TextBook/ch10/10.2.py
@@ -14,3 +14,10 @@ print('성년 리스트 : ')
 for a in filter(lambda x: x>=19,ages):
     print(a,end =' ')
 
+ages=[34,39,20,18,13,54]
+adult_ages = list(filter(lambda x:x>=19,ages))
+print(adult_ages)
+
+ages=[34,39,20,18,13,54]
+adult_ages1=list(filter(lambda x:x>=19,ages))
+print(adult_ages1)
