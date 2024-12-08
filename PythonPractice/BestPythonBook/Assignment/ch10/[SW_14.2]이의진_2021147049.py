d1f5b5326b27e8611e9ddd30b16bc7c06678e28a
Jin <Dowha132@yonsei.ac.kr>
2024-12-08 17:30:55 +0900

    [fix]14.2

--- PythonPractice/BestPythonBook/Assignment/ch10/[SW_14.2]이의진_2021147049.py
+++ PythonPractice/BestPythonBook/Assignment/ch10/[SW_14.2]이의진_2021147049.py
@@ -10,4 +10,17 @@ for i in num:
     if is_even(i):
         E1_list.append(i)
 
+E2_list=[]
+for b in filter(lambda n: n%2==0,num):
+    E2_list.append(b)
+
+#[표현식 for 변수 in 반복자/연속열]
+E3_list=[x for x in num if x%2==0]
+
+E4_list=list(filter(lambda n:n%2==0,num))
+
+
 print(f"E1_list = {E1_list}")
+print(f"E2_list={E2_list}")
+print(f"E3_list={E3_list}")
+print(f"E4_list={E4_list}")
