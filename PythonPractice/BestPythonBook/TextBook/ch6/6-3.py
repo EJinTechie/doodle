23e30c162ce31ab224cfca522ae2762747e466fe
Jin <Dowha132@yonsei.ac.kr>
2024-12-09 10:02:24 +0900

    [add]6-3

---
+++ PythonPractice/BestPythonBook/TextBook/ch6/6-3.py
@@ -0,0 +1,5 @@
+menu={'Americano' : 3000, 'Cappuccino': 3500,'Cafe Latte':4000 , 'Espresso':3600}
+print(f"Americano Price :{menu['Americano']}")
+print(f"Cappuccino Price :{menu['Cappuccino']}")
+print(f"Cafe Latte Price :{menu['Cafe Latte']}")
+print(f"Espresso Price :{menu['Espresso']}")
