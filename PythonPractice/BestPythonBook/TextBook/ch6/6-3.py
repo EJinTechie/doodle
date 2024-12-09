725dd76c44dc7c6ac7b525d4cf65150a35894eed
Jin <Dowha132@yonsei.ac.kr>
2024-12-09 10:05:27 +0900

    [add]6-3

--- PythonPractice/BestPythonBook/TextBook/ch6/6-3.py
+++ PythonPractice/BestPythonBook/TextBook/ch6/6-3.py
@@ -1,5 +1,12 @@
+#1
 menu={'Americano' : 3000, 'Cappuccino': 3500,'Cafe Latte':4000 , 'Espresso':3600}
 print(f"Americano Price :{menu['Americano']}")
 print(f"Cappuccino Price :{menu['Cappuccino']}")
 print(f"Cafe Latte Price :{menu['Cafe Latte']}")
 print(f"Espresso Price :{menu['Espresso']}")
+print('\n')
+
+#2
+menu1=input("choose from above menu : ")
+price=menu[menu1]
+print(f"{menu1} is {price}. To go?")
