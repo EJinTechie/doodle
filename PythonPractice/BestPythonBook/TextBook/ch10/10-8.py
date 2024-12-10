c735cbb1d3faac3ff49bb01e99ef3dfc98dc9de8
Jin <Dowha132@yonsei.ac.kr>
2024-12-10 18:10:29 +0900

    [add]10-8

--- PythonPractice/BestPythonBook/TextBook/ch10/10-8.py
+++ PythonPractice/BestPythonBook/TextBook/ch10/10-8.py
@@ -2,19 +2,25 @@ n_list=[-22.3,29.44,902.2,45.7,-887.1,-56.3]
 print(f"최댓값 : {max(n_list)}")
 print(f"최솟값 : {min(n_list)}")
 
-def my_min(numbers):
-    min_value=numbers[0]
-    for num in numbers:
-        if num<min_value:
-            min_value = num
-    return min_value
+def my_max(x):
+    max_v=x[0]
+    for num in x:
+        if num > max_v:
+            max_v=num
+    return max_v
 
-def my_max(numbers):
-    max_value =numbers[0]
-    for num in numbers:
-        if num>max_value:
-            max_value=num
-    return max_value
+def my_min(x):
+    min_v=x[0]
+    for num in x:
+        if num < min_v:
+            min_v=num
+    return min_v
 
-print(my_max(n_list))
-print(my_min(n_list))
+print(f"최댓값 : {my_max(n_list)}")
+print(f"최솟값 : {my_min(n_list)}")
+
+from functools import reduce
+max_value=reduce(lambda x,y :x if x>y else y,n_list)
+min_value=reduce(lambda x,y :x if x<y else y,n_list)
+print(f"최댓값 : {max_value}")
+print(f"최솟값 : {min_value}")
